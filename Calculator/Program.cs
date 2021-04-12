fd876115b25db962591cbd055be6caa0a47d58c2
Stewart Charles Fisher <25020928@students.lincoln.ac.uk>
2021-04-12 20:26:20 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Welcome the user to the application
+            Console.WriteLine("Hello, my name is Descartes. I am a calculator capable of simple calculations.\n");
+            Console.WriteLine("Press ENTER to begin\nInput E to exit\n");
+            switch (Console.ReadLine().ToLower())
+            {
+
+            }
+        }
+    }
+}
